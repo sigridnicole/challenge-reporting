0264a118aa4fca237a83ee826c3bcc5f60824498
sigridnicole <sigridnicoler@gmail.com>
2022-12-08 03:02:15 +0800

    test: update expected student data

--- test.js
+++ test.js
@@ -25,7 +25,23 @@ tape('get student by id | [success]', async function (t) {
   const url = `${endpoint}/student/${studentId}`
   try {
     const { data } = await jsonist.get(url)
-    const expectedStudentData = { id: 1, first_name: 'John', last_name: 'Doe' }
+    const expectedStudentData = {
+      id: 1,
+      first_name: 'Scotty',
+      last_name: 'Quigley',
+      email: 'Scotty79@hotmail.com',
+      is_registered: 1,
+      is_approved: 1,
+      password_hash: '657907e1fd8e48e2be2aa59031ff8e0f0ecf8694',
+      address: '241 Denesik Knolls Apt. 955',
+      city: 'Buffalo',
+      state: 'ME',
+      zip: '04710',
+      phone: '1-503-560-6954',
+      created: '1628767983203.0',
+      last_login: '1628770445749.0',
+      ip_address: '2.137.18.155'
+    }
 
     t.deepEqual(data.student, expectedStudentData, 'should get expected student data')
     t.end()
@@ -54,8 +70,29 @@ tape('get student grades report | [success]', async function (t) {
   try {
     const { data } = await jsonist.get(url)
 
-    const expectedStudentData = { id: 1, first_name: 'John', last_name: 'Doe' }
-    const expectedStudentGrades = [{ id: 1, course: 'Calculus', grade: 50 }, { id: 1, course: 'Microeconomics', grade: 43 }, { id: 1, course: 'Statistics', grade: 50 }, { id: 1, course: 'Astronomy', grade: 63 }]
+    const expectedStudentData = {
+      id: 1,
+      first_name: 'Scotty',
+      last_name: 'Quigley',
+      email: 'Scotty79@hotmail.com',
+      is_registered: 1,
+      is_approved: 1,
+      password_hash: '657907e1fd8e48e2be2aa59031ff8e0f0ecf8694',
+      address: '241 Denesik Knolls Apt. 955',
+      city: 'Buffalo',
+      state: 'ME',
+      zip: '04710',
+      phone: '1-503-560-6954',
+      created: '1628767983203.0',
+      last_login: '1628770445749.0',
+      ip_address: '2.137.18.155'
+    }
+    const expectedStudentGrades = [
+      { id: 1, course: 'Calculus', grade: 50 },
+      { id: 1, course: 'Microeconomics', grade: 43 },
+      { id: 1, course: 'Statistics', grade: 50 },
+      { id: 1, course: 'Astronomy', grade: 63 }
+    ]
 
     t.deepEqual(data.student, expectedStudentData, 'should get expected student data')
     t.deepEqual(data.studentGrades, expectedStudentGrades, 'should get expected student grades')
